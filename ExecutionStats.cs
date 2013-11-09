6d63c463ab442dd6b6502e1b1dcfe1d37f594463
David Roberts <david.roberts63@gmail.com>
2013-11-08 19:40:00 -0600

    Test tab to work with Glimpse

---
+++ ExecutionStats.cs
@@ -0,0 +1,32 @@
+﻿using Glimpse.Core.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatsdNet.Glimpse.Execution
+{
+    public class ExecutionStats : TabBase, ITabSetup
+    {
+        public override object GetData(ITabContext context)
+        {
+            var results = new Dictionary<string, int>();
+
+            results.Add("Test", 1);
+
+            return results;
+        }
+
+        public override string Name
+        {
+            get { return "Statistics"; }
+        }
+
+        public void Setup(ITabSetupContext context)
+        {
+            // no op currently.
+        }
+
+    }
+}
